266bfde8e8464687cd6a9a0ab47e07d01b7e1321
normalman159 <binben279@gmail.com>
2023-11-23 13:09:13 +0700

    update

--- src/alart-popup.js
+++ src/alart-popup.js
@@ -1,46 +1,52 @@
-import React from 'react'
+import React from 'react';
+import Popup from 'reactjs-popup';
 
-//import Alert from 'react-popup-alert'
-const Popup = () => {
-  const [alert, setAlert] = React.useState({
-    type: 'success',
-    text: 'Hãy nghỉ ngơi và quay lại chơi vào ngày mai nhé',
-    show: true
-  })
-
-  function onCloseAlert() {
-    setAlert({
-      type: 'success',
-      text: 'Hãy nghỉ ngơi và quay lại chơi vào ngày mai nhé',
-      show: true
-    })
-    window.open("https://coco-game-hackathon.vercel.app", '_blank');
-  }
-  return (
-    <div>
-      {/* <div>
-        <Alert
-          header={'Bạn đã hết thời gian chơi hôm nay'}
-          btnText={'Quay lại trang chủ'}
-          text={alert.text}
-          type={alert.type}
-          show={alert.show}
-          onClosePress={onCloseAlert}
-          pressCloseOnOutsideClick={false}
-          showBorderBottom={true}
-          alertStyles={{}}
-          headerStyles={{}}
-          textStyles={{
-
-          }}
-          buttonStyles={{
-            
-          }}
-        />
-      </div> */}
-
-    </div>
-  )
-}
-
-export default Popup
+export default function Pop_up  (){
+  <Popup
+    trigger={<button className="button"> Open Modal </button>}
+    modal
+    nested
+  >
+    {close => (
+      <div className="modal">
+        <button className="close" onClick={close}>
+          &times;
+        </button>
+        <div className="header"> Modal Title </div>
+        <div className="content">
+          {' '}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
+          Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
+          delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
+          commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
+          explicabo nemo nam libero ad, doloribus, voluptas rem alias. Vitae?
+        </div>
+        <div className="actions">
+          <Popup
+            trigger={<button className="button"> Trigger </button>}
+            position="top center"
+            nested
+          >
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+              magni omnis delectus nemo, maxime molestiae dolorem numquam
+              mollitia, voluptate ea, accusamus excepturi deleniti ratione
+              sapiente! Laudantium, aperiam doloribus. Odit, aut.
+            </span>
+          </Popup>
+          <button
+            className="button"
+            onClick={() => {
+              console.log('modal closed ');
+              close();
+            }}
+          >
+            close modal
+          </button>
+        </div>
+      </div>
+    )}
+  </Popup>
+};
